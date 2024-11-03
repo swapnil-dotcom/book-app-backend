4ba89cd3c211db1e1d04b753a62976b0244beec1
swapnil-dotcom <73781912+swapnil-dotcom@users.noreply.github.com>
2024-11-04 01:04:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 // middleware
+app.use((req, res, next) => {
+  if (req.url === '/favicon.ico') {
+    res.status(204).end(); // Respond with "No Content" and end the request
+  } else {
+    next();
+  }
+});
 app.use(express.json());
 app.use(cors({
     origin: ['http://localhost:5173', 'https://book-app-frontend-kohl.vercel.app'],
